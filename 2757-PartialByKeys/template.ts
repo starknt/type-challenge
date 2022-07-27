500bad41e7d06c14bd4a4f880162bb1836fe2959
starknt <1431880400@qq.com>
2022-07-27 11:14:33 +0800

    chore: 2757-RequiredByKeys

--- 2757-PartialByKeys/template.ts
+++ 2757-PartialByKeys/template.ts
@@ -1,4 +1,4 @@
-type PartialByKeys<O extends Record<string, any>, U extends any = keyof O, PO = Partial<PickByType<O, O[U]>>> =
+type PartialByKeys<O extends Record<string, any>, U extends any = keyof O, UU = U extends keyof O ? U : never, PO = Partial<Pick<O, UU>>> =
   Expand<PO & {
     [K in keyof O as K extends U ? never : K]: O[K]
   }>
---
+++ 2759-RequiredByKeys/template.ts
@@ -0,0 +1,10 @@
+type RequiredByKeys<O extends Record<string, any>, U = keyof O, UU = U extends keyof O ? U : never, RO = Required<Pick<O, UU>>> =
+  Expand<RO & O>
+
+interface User {
+  name?: string
+  age?: number
+  address?: string
+}
+
+type UserRequiredName = RequiredByKeys<User, 'name' | 'age' | '123'> // { name: string; age?: number; address?: string }
