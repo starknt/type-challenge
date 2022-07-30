cfb1c6fa5f967dc2b45e046fa9042524e8b63b7b
starknt <1431880400@qq.com>
2022-07-30 10:38:45 +0800

    chore: 3326 other method

--- 3326-BEM-style-string/template.ts
+++ 3326-BEM-style-string/template.ts
@@ -1,7 +1,12 @@
-type BEM<B extends string, E extends string[], M extends string[], Mode = 'b'> =
-  E extends [infer EF extends string, ...infer ER extends string[]] ? BEM<`${B}__${EF}`, ER, M>
-  : M extends [infer MF extends string, ...infer MR extends string[]] ? `${B}--${MF}` | BEM<B, E, MR, 'm'>
-  : Mode extends 'b' ? B : never
+// type BEM<B extends string, E extends string[], M extends string[], Mode = 'b'> =
+//   E extends [infer EF extends string, ...infer ER extends string[]] ? BEM<`${B}__${EF}`, ER, M>
+//   : M extends [infer MF extends string, ...infer MR extends string[]] ? `${B}--${MF}` | BEM<B, E, MR, 'm'>
+//   : Mode extends 'b' ? B : never
+
+type BEM<B extends string, E extends string[], M extends string[]> =
+  E extends [] ? `${B}--${M[number]}`
+  : M extends [] ? `${B}__${E[number]}`
+  : `${B}__${E[number]}--${M[number]}`
 
 
 type BEM_ = BEM<'btn', ['price'], []> // print btn__price
