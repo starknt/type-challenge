cb06487700348b394e8479a61b7c6d6935321add
starknt <1431880400@qq.com>
2022-07-31 09:19:00 +0800

    fix: 3376 type error

--- 3376-InorderTraversal/template.ts
+++ 3376-InorderTraversal/template.ts
@@ -4,13 +4,5 @@ interface TreeNode {
   right: TreeNode | null
 }
 
-interface Tree {
-  val: any
-  left: TreeNode
-  right: TreeNode
-}
-
-// @ts-ignore
-type InorderTraversal<T, L = T['left'], R = T['right']> =
-  // @ts-ignore
-  T extends null ? [] : [...L extends null ? [] : InorderTraversal<L>, T['val'], ...R extends null ? [] : InorderTraversal<R>]
+type InorderTraversal<T extends TreeNode | null> =
+  [T] extends [TreeNode] ? [...[T['left']] extends [TreeNode] ? InorderTraversal<T['left']> : [], T['val'], ...[T['right']] extends [TreeNode] ? InorderTraversal<T['right']> : []] : []
